fe6212736fba40eb267f45ebbf4f14e13f382fe9
Soyeon Kim <sylille96@gmail.com>
2025-08-25 20:36:23 +0900

    speed-insight add

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
-export const metadata = { title: 'My Chat', description: 'Inoculation Chat Beta' }
+export const metadata = { title: 'Inoculation Chat', description: 'Inoculation Chat Beta' }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
